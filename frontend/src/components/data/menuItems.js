09692af34cf1a16f53c8750763148008f2e47656
TorenUK <td_fraser@hotmail.co.uk>
2020-12-01 20:37:35 +0000

    spelling error

--- frontend/src/components/data/menuItems.js
+++ frontend/src/components/data/menuItems.js
@@ -12,7 +12,7 @@ export const starterItems = [
       "https://images.unsplash.com/photo-1590828413506-d632c9e582c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1625&q=80",
   },
   {
-    name: "Vegetairan Samosas",
+    name: "Vegetarian Samosas",
     price: 2.99,
     image:
       "https://images.unsplash.com/photo-1601050690597-df0568f70950?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
